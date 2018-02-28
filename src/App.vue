d96d042412bff3178aee2d99ece66b76597a2faa
Darko Bistanovski <bistanovskidarko@gmail.com>
2018-02-28 21:58:42 +0100

    Switching to Vue.js

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h2>Shift Illuminator</h2>
+    <div>
+      <el-button @click="startHacking">Start</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'ShiftIlluminator',
+        type: 'success',
+        message: 'Let\'s illuminate awesome things!',
+        duration: 3000
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
